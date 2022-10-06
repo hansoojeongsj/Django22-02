2cf7a27a77248be59c51e3bf7469ea1231dfdb7c
unknown <tnwjd3758@duksung.ac.kr>
2022-10-06 14:21:18 +0900

    포스트 모델에 file_upload 추가하기

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
     #대문자 Y는 2022인 네 글자를 표현, 소문자 m,d는 앞에꺼 빼고 뒤에 두 글자
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
