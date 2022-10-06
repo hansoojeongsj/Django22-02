b8bad37a33609c0df7557dc7818fc25f49e98960
unknown <tnwjd3758@duksung.ac.kr>
2022-10-06 14:07:06 +0900

    포스트 목록/상세 페이지에 대표이미지 출력하기

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,9 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
+    #대문자 Y는 2022인 네 글자를 표현, 소문자 m,d는 앞에꺼 빼고 뒤에 두 글자
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
