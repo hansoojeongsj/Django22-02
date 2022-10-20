834853684c27154dd0a2bce7ef56c2c47f73db2f
unknown <tnwjd3758@duksung.ac.kr>
2022-10-20 13:48:29 +0900

    네비게이션바, 사이드바, 푸터 모듈화

--- blog/tests.py
+++ blog/tests.py
@@ -8,6 +8,12 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def nav_test(self, soup):
+        # navbar 정상적?
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('AboutMe', navbar.text)
+
     def test_post_list(self):
         response = self.client.get('/blog/')
         # response 결과가 정상적인지 확인해야함.
@@ -17,11 +23,11 @@ class TestView(TestCase):
         # title이 정상적으로 보이는지
         self.assertEqual(soup.title.text, 'Blog')
 
-        navbar = soup.nav
         # navbar가 정상적으로 보이는지
-        self.assertIn('Blog', navbar.text)
-        # Post가 정상적으로 보이는지
-        self.assertIn('AboutMe', navbar.text)
+        #navbar = soup.nav
+        #self.assertIn('Blog', navbar.text)
+        #self.assertIn('AboutMe', navbar.text)
+        self.nav_test(soup)
 
         # 1.맨 처음엔 Post가 하나도 안 보임
         self.assertEqual(Post.objects.count(),0);
@@ -41,7 +47,7 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
         self.assertNotIn('아무 게시물이 없습니다.', main_area.text)
 
-        def test_post_detail(self):
+    def test_post_detail(self):
             post_001 = Post.objects.create(title="첫번째 포스트", content="첫번째 포스트 입니다.")
             self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
 
@@ -49,13 +55,14 @@ class TestView(TestCase):
             self.assertEqual(response.status_code, 200)
             soup = BeautifulSoup(response.content, 'html.parser')
 
-            navbar = soup.nav
-            self.assertIn('Blog', navbar.text)
-            self.assertIn('AboutMe', navbar.text)
+            self.nav_test(soup)
+            #navbar = soup.nav
+            #self.assertIn('Blog', navbar.text)
+            #self.assertIn('AboutMe', navbar.text)
 
             self.assertIn(post_001.title, soup.title.text)
 
-            main_Area = soup.find('div', id='main-area')
+            main_area = soup.find('div', id='main-area')
             post_area = main_area.find('div', id='post-area')
             self.assertIn(post_001.title, post_area.text)
             self.assertIn(post_001.content, post_area.text)
