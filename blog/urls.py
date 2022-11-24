d60c6f6043aea800e7b7608559e5f5d6487bc091
unknown <tnwjd3758@duksung.ac.kr>
2022-11-24 14:31:10 +0900

    검색 기능 구현하기

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[  # IP주소/blog/
-    path('', views.PostList.as_view()), #urls.py와 views.py와 템플릿파일로 블로그index페이지만들기2
-                                        #ListView로 포스트목록페이지 만들기
+    path('', views.PostList.as_view()),   #urls.py와 views.py와 템플릿파일로 블로그index페이지만들기2
+                                          #ListView로 포스트목록페이지 만들기
     path('<int:pk>/', views.PostDetail.as_view()),  #DetailView로포스트상세페이지만들기
     path('<int:pk>/new_comment/', views.new_comment),
     path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
@@ -12,7 +12,8 @@ urlpatterns=[  # IP주소/blog/
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('category/<str:slug>/', views.category_page), #IP주소/blog/category/slug/
 
-    path('tag/<str:slug>/', views.tag_page) #IP주소/blog/tag/slug/
+    path('tag/<str:slug>/', views.tag_page),    #IP주소/blog/tag/slug/
+    path('search/<str:q>/', views.PostSearch.as_view())  #검색
 
     #path('', views.index), # IP주소/blog
     #path('<int:pk>/', views.single_post_page)
